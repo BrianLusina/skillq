bd49d87d52a62f8b5d5d4d99c33aa63145f4d68f
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2024-04-17 21:45:47 +0300

    feat(utils): validator utils

--- server/utils/validators/validators.go
+++ server/utils/validators/validators.go
@@ -1,12 +1,14 @@
-// Package validators contain helper functions for validation of fields and values
 package validators
 
 import (
+	"errors"
 	"fmt"
+	"reflect"
 	"regexp"
 
-	validator "github.com/go-ozzo/ozzo-validation"
+	ozzoValidation "github.com/go-ozzo/ozzo-validation"
 	"github.com/go-ozzo/ozzo-validation/is"
+	"github.com/go-playground/validator/v10"
 )
 
 var (
@@ -18,7 +20,7 @@ var (
 
 // ValidateUrl checks if the provided URL is a valid URL
 func ValidateUrl(url string) error {
-	err := validator.Validate(url, validator.Required, is.URL)
+	err := ozzoValidation.Validate(url, ozzoValidation.Required, is.URL)
 	if err != nil {
 		return fmt.Errorf("provided url: %s is invalid", url)
 	}
@@ -35,7 +37,7 @@ func ValidatePhoneNumber(phoneNumber string) error {
 
 // ValidateEmail validates an email address returning an error if invalid
 func ValidateEmail(email string) error {
-	err := validator.Validate(email, validator.Required, is.Email)
+	err := ozzoValidation.Validate(email, ozzoValidation.Required, is.Email)
 	if err != nil {
 		return fmt.Errorf("provided email address: %s is invalid", email)
 	}
@@ -44,7 +46,7 @@ func ValidateEmail(email string) error {
 
 // ValidateCountryCode validates a country code
 func ValidateCountryCode(country string) error {
-	err := validator.Validate(country, is.CountryCode3)
+	err := ozzoValidation.Validate(country, is.CountryCode3)
 
 	if err != nil {
 		return fmt.Errorf("provided country code %s is not a valid ISO3166 Alpha 3 country code", country)
@@ -55,11 +57,51 @@ func ValidateCountryCode(country string) error {
 
 // ValidateCurrencyCode validates that a given currency code follows ISO 4217 code
 func ValidateCurrencyCode(code string) error {
-	err := validator.Validate(code, is.CurrencyCode)
+	err := ozzoValidation.Validate(code, is.CurrencyCode)
 
 	if err != nil {
 		return fmt.Errorf("provided currency code %s is not a valid ISO4217 currency code", code)
 	}
 
 	return nil
+}
+
+// GetValidationErrMsg checks to see if the provided err is a validation error and
+// returns the first validation error message.
+func GetValidationErrMsg(s any, err error) (errMsg string) {
+	fieldErrors := validator.ValidationErrors{}
+
+	if ok := errors.As(err, &fieldErrors); ok {
+		fieldErr := fieldErrors[0]
+		fieldName := getStructTag(s, fieldErr.Field(), "json")
+
+		switch fieldErr.Tag() {
+		case "required":
+			errMsg = fmt.Sprintf("%s is a required field", fieldName)
+		default:
+			errMsg = fmt.Sprintf("Invalid input on %s", fieldName)
+		}
+	}
+
+	return errMsg
+}
+
+func getStructTag(s any, fieldName string, tagKey string) string {
+	t := reflect.TypeOf(s)
+	field, found := t.FieldByName(fieldName)
+
+	if t.Kind() != reflect.Struct {
+		return fieldName
+	}
+
+	if !found {
+		return fieldName
+	}
+
+	return field.Tag.Get(tagKey)
+}
+
+// IsValidationError checks to see if error is of type validator.ValidationErrors.
+func IsValidationError(err error) bool {
+	return errors.As(err, &validator.ValidationErrors{})
 }
